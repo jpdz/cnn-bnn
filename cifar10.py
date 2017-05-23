1c81fca8bd36ddd29eacdb8b31cfb4d463f0b429
yw774 <yw774@cornell.edu>
2017-05-22 21:04:22 -0400

    add cifar10 train

---
+++ cifar10.py
@@ -0,0 +1,323 @@
+# Copyright 2015 Matthieu Courbariaux
+
+# This file is part of BinaryConnect.
+
+# BinaryConnect is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# BinaryConnect is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with BinaryConnect.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import sys
+import os
+import time
+
+import numpy as np
+np.random.seed(1234) # for reproducibility?
+
+
+import theano
+import theano.tensor as T
+
+import lasagne
+
+import cPickle as pickle
+import gzip
+
+import batch_norm
+import binary_connect
+
+from collections import OrderedDict
+
+from utils.inputdata import *
+from utils.Gates import Gates
+
+if __name__ == "__main__":
+    
+    # BN parameters
+    batch_size = 50
+    print("batch_size = "+str(batch_size))
+    # alpha is the exponential moving average factor
+    alpha = .1
+    print("alpha = "+str(alpha))
+    epsilon = 1e-4
+    print("epsilon = "+str(epsilon))
+    
+    # Training parameters
+    num_epochs = 1
+    print("num_epochs = "+str(num_epochs))
+    
+    # BinaryConnect    
+    binary = True
+    print("binary = "+str(binary))
+    stochastic = True
+    print("stochastic = "+str(stochastic))
+    # (-H,+H) are the two binary values
+    # H = "Glorot"
+    H = 1.
+    print("H = "+str(H))
+    # W_LR_scale = 1.    
+    W_LR_scale = "Glorot" # "Glorot" means we are using the coefficients from Glorot's paper
+    print("W_LR_scale = "+str(W_LR_scale))
+    
+    # Decaying LR 
+    LR_start = 0.003
+    print("LR_start = "+str(LR_start))
+    LR_fin = 0.000002
+    print("LR_fin = "+str(LR_fin))
+    LR_decay = (LR_fin/LR_start)**(1./num_epochs)
+    print("LR_decay = "+str(LR_decay))
+    # BTW, LR decay might good for the BN moving average...
+
+    
+    # extract data
+    gates = Gates(force=True, image_size = 28, inputsize1=900, inputsize2=900, mask = False)
+
+    gates.train.stack()
+    gates.test.stack()
+    gates.valid.stack()
+
+    gates.train.resize2()
+    gates.test.resize2()
+    gates.valid.resize2()
+
+    print(gates.train.X.shape)
+
+    gates.train.X = gates.train.X.reshape(-1,1,32,32)
+    gates.valid.X = gates.valid.X.reshape(-1,1,32,32)
+    gates.test.X  = gates.test.X.reshape(-1,1,32,32)
+
+    print(len(gates.train.X))
+    
+    
+    # for hinge loss
+    gates.train.Y = 2* gates.train.Y - 1.
+    gates.valid.Y = 2* gates.valid.Y - 1.
+    gates.test.Y = 2* gates.test.Y - 1.
+
+    print('Building the CNN...') 
+    
+    # Prepare Theano variables for inputs and targets
+    input = T.tensor4('inputs')
+    target = T.matrix('targets')
+    LR = T.scalar('LR', dtype=theano.config.floatX)
+
+    cnn = lasagne.layers.InputLayer(
+            shape=(None, 1, 32, 32),
+            input_var=input)
+    
+    # 128C3-128C3-P2             
+    cnn = binary_connect.Conv2DLayer(
+            cnn, 
+            binary=binary,
+            stochastic=stochastic,
+            H=H,
+            W_LR_scale=W_LR_scale,
+            num_filters=128, 
+            filter_size=(3, 3),
+            pad=1,
+            nonlinearity=lasagne.nonlinearities.identity) 
+
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify) 
+            
+    cnn = binary_connect.Conv2DLayer(
+            cnn, 
+            binary=binary,
+            stochastic=stochastic,
+            H=H,
+            W_LR_scale=W_LR_scale,
+            num_filters=128, 
+            filter_size=(3, 3),
+            pad=1,
+            nonlinearity=lasagne.nonlinearities.identity)
+    
+    cnn = lasagne.layers.MaxPool2DLayer(cnn, pool_size=(2, 2))
+    
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify)
+            
+    # 256C3-256C3-P2             
+    cnn = binary_connect.Conv2DLayer(
+            cnn, 
+            binary=binary,
+            stochastic=stochastic,
+            H=H,
+            W_LR_scale=W_LR_scale,
+            num_filters=256, 
+            filter_size=(3, 3),
+            pad=1,
+            nonlinearity=lasagne.nonlinearities.identity)
+    
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify)
+            
+    cnn = binary_connect.Conv2DLayer(
+            cnn, 
+            binary=binary,
+            stochastic=stochastic,
+            H=H,
+            W_LR_scale=W_LR_scale,
+            num_filters=256, 
+            filter_size=(3, 3),
+            pad=1,
+            nonlinearity=lasagne.nonlinearities.identity)
+    
+    cnn = lasagne.layers.MaxPool2DLayer(cnn, pool_size=(2, 2))
+    
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify)
+            
+    # 512C3-512C3-P2              
+    cnn = binary_connect.Conv2DLayer(
+            cnn, 
+            binary=binary,
+            stochastic=stochastic,
+            H=H,
+            W_LR_scale=W_LR_scale,
+            num_filters=512, 
+            filter_size=(3, 3),
+            pad=1,
+            nonlinearity=lasagne.nonlinearities.identity)
+    
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify)
+                  
+    cnn = binary_connect.Conv2DLayer(
+            cnn, 
+            binary=binary,
+            stochastic=stochastic,
+            H=H,
+            W_LR_scale=W_LR_scale,
+            num_filters=512, 
+            filter_size=(3, 3),
+            pad=1,
+            nonlinearity=lasagne.nonlinearities.identity)
+
+    
+    cnn = lasagne.layers.MaxPool2DLayer(cnn, pool_size=(2, 2))
+    
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify)
+
+    
+    # 1024FP-1024FP-7FP            
+    cnn = binary_connect.DenseLayer(
+                cnn, 
+                binary=binary,
+                stochastic=stochastic,
+                H=H,
+                W_LR_scale=W_LR_scale,
+                nonlinearity=lasagne.nonlinearities.identity,
+                num_units=1024)
+
+             
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify)
+            
+    cnn = binary_connect.DenseLayer(
+                cnn, 
+                binary=binary,
+                stochastic=stochastic,
+                H=H,
+                W_LR_scale=W_LR_scale,
+                nonlinearity=lasagne.nonlinearities.identity,
+                num_units=1024)      
+                  
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.rectify)
+    
+    cnn = binary_connect.DenseLayer(
+                cnn, 
+                binary=binary,
+                stochastic=stochastic,
+                H=H,
+                W_LR_scale=W_LR_scale,
+                nonlinearity=lasagne.nonlinearities.identity,
+                num_units=7)      
+                  
+    cnn = batch_norm.BatchNormLayer(
+            cnn,
+            epsilon=epsilon, 
+            alpha=alpha,
+            nonlinearity=lasagne.nonlinearities.identity)
+
+    train_output = lasagne.layers.get_output(cnn, deterministic=False)
+    
+    # squared hinge loss
+    loss = T.mean(T.sqr(T.maximum(0.,1.-target*train_output)))
+    
+    if binary:
+        
+        # W updates
+        W = lasagne.layers.get_all_params(cnn, binary=True)
+        W_grads = binary_connect.compute_grads(loss,cnn)
+        updates = lasagne.updates.adam(loss_or_grads=W_grads, params=W, learning_rate=LR)
+        updates = binary_connect.clipping_scaling(updates,cnn)
+        
+        # other parameters updates
+        params = lasagne.layers.get_all_params(cnn, trainable=True, binary=False)
+        updates = OrderedDict(updates.items() + lasagne.updates.adam(loss_or_grads=loss, params=params, learning_rate=LR).items())
+        
+    else:
+        params = lasagne.layers.get_all_params(cnn, trainable=True)
+        updates = lasagne.updates.adam(loss_or_grads=loss, params=params, learning_rate=LR)
+
+    test_output = lasagne.layers.get_output(cnn, deterministic=True)
+    test_loss = T.mean(T.sqr(T.maximum(0.,1.-target*test_output)))
+    test_err = T.mean(T.neq(T.argmax(test_output, axis=1), T.argmax(target, axis=1)),dtype=theano.config.floatX)
+    
+    # Compile a function performing a training step on a mini-batch (by giving the updates dictionary) 
+    # and returning the corresponding training loss:
+    train_fn = theano.function([input, target, LR], loss, updates=updates)
+
+    # Compile a second function computing the validation loss and accuracy:
+    val_fn = theano.function([input, target], [test_loss, test_err])
+
+    print('Training...')
+    
+    print(gates.train.X.shape)
+    binary_connect.train(
+            train_fn,
+            val_fn,
+            batch_size,
+            LR_start,LR_decay,
+            num_epochs,
+            gates.train.X,gates.train.Y,
+            gates.valid.X,gates.valid.Y,
+            gates.test.X,gates.test.Y)
+   
+    np.savez('model_all.npz', lasagne.layers.get_all_param_values(cnn))
